4cc21d68cbb0304daccfe98feba52a0c6e669786
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-16 14:39:49 +0530

    initial commit

---
+++ src/test/java/com/online_milk_store/upi_npcl_microservice/UpiNpclMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_milk_store.upi_npcl_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiNpclMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
