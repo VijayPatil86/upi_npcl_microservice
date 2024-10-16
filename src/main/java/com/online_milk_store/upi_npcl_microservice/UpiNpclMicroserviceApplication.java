f02b490f547226b15725c89c9fd88e1b66fd80e4
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-16 14:38:19 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/upi_npcl_microservice/UpiNpclMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.online_milk_store.upi_npcl_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiNpclMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiNpclMicroserviceApplication.class, args);
+		System.out.println("success... UPI NPCL service...");
+	}
+
+}
