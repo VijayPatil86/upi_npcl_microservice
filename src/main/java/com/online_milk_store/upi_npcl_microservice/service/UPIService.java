ecd16a1b0a9be60e1d183164dafc391f536a0e3a
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-16 17:25:48 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/upi_npcl_microservice/service/UPIService.java
@@ -0,0 +1,41 @@
+package com.online_milk_store.upi_npcl_microservice.service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.online_milk_store.upi_npcl_microservice.bean.UpiPaymentInfoBean;
+import com.online_milk_store.upi_npcl_microservice.entity.UpiPaymentInfoEntity;
+import com.online_milk_store.upi_npcl_microservice.repository.UPINPCLTransactionRepository;
+
+@Service
+@Transactional
+public class UPIService {
+	final static private Logger LOGGER = LogManager.getLogger(UPIService.class);
+
+	private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+	@Autowired
+	private UPINPCLTransactionRepository upinpclTransactionRepository;
+
+	public void savePaymentRequest(UpiPaymentInfoBean upiPaymentInfoBean) {
+		LOGGER.debug("UPIService.savePaymentRequest() --- START");
+		LOGGER.info("UPIService.savePaymentRequest() --- upiPaymentInfoBean: " + upiPaymentInfoBean);
+		UpiPaymentInfoEntity upiPaymentInfoEntityToSave = UpiPaymentInfoEntity.builder()
+				.upiId(upiPaymentInfoBean.getUpiId())
+				.amount(upiPaymentInfoBean.getAmount())
+				.payerTransactionId(upiPaymentInfoBean.getPayerTransactionId())
+				.payerTransactionTimestamp(Timestamp.valueOf(LocalDateTime.parse(upiPaymentInfoBean.getPayerTransactionTimestamp(), formatter)))
+				.build();
+		LOGGER.info("UPIService.savePaymentRequest() --- upiPaymentInfoEntityToSave: " + upiPaymentInfoEntityToSave);
+		UpiPaymentInfoEntity upiPaymentInfoEntitySaved = upinpclTransactionRepository.save(upiPaymentInfoEntityToSave);
+		LOGGER.info("UPIService.savePaymentRequest() --- upiPaymentInfoEntitySaved: " + upiPaymentInfoEntitySaved);
+		LOGGER.debug("UPIService.savePaymentRequest() --- END");
+	}
+}
